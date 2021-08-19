5c6ec19fed32002de26e164fe47f5ce371907443
PrinceK <princephpdev@gmail.com>
2021-08-19 23:47:34 +0530

    Added validations

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,8 +1,11 @@
 import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import User from "App/Models/User";
+import LoginValidator from "App/Validators/LoginValidator";
+import RegisterValidator from "App/Validators/RegisterValidator";
 
 export default class AuthController {
   public async login({ request, auth }: HttpContextContract) {
+    await request.validate(LoginValidator)
     const email = request.input("email");
     const password = request.input("password");
     const token = await auth.use("api").attempt(email, password, {
@@ -11,6 +14,7 @@ export default class AuthController {
     return token.toJSON();
   }
   public async register({ request, auth }: HttpContextContract) {
+    await request.validate(RegisterValidator)
     const email = request.input("email");
     const password = request.input("password");
     const newUser = new User();
@@ -23,10 +27,10 @@ export default class AuthController {
     return token.toJSON();
   }
 
-  public async logout({ auth}: HttpContextContract) {
+  public async logout({ auth }: HttpContextContract) {
     await auth.use('api').revoke()
-      return {
-        message: "user logged out successfully"
-      }
+    return {
+      message: "user logged out successfully"
+    }
   }
 }
